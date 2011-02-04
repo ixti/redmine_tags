9c39a8a85dfa6593840894cb8e0b8ad2ca4ab272
Aleksey V Zapparov <ixti@member.fsf.org>
2011-02-04 01:28:11 +0100

    Add available_tags logic

--- lib/redmine_tags/patches/issue_patch.rb
+++ lib/redmine_tags/patches/issue_patch.rb
@@ -12,8 +12,37 @@ module RedmineTags
       end
 
       module ClassMethods
-        def available_tags(options)
-          Issue.tag_counts
+        # Returns available issue tags
+        # === Parameters
+        # * <i>options</i> = (optional) Options hash of
+        #   * project   - Project to search in.
+        #   * open_only - Boolean. Whenever search within open issues only.
+        #   * name_like - String. Substring to filter found tags.
+        def available_tags(options = {})
+          project   = options[:project]
+          open_only = options[:open_only]
+          name_like = options[:name_like]
+          options   = {}
+          visible   = ARCondition.new
+          
+          if project
+            project = project.id if project.is_a? Project
+            visible << ["#{Issue.table_name}.project_id = ?", project]
+          end
+
+          if open_only
+            visible << ["#{Issue.table_name}.status_id IN " +
+                        "( SELECT issue_status.id " + 
+                        "    FROM #{IssueStatus.table_name} issue_status " +
+                        "   WHERE issue_status.is_closed = ? )", false]
+          end
+
+          if name_like
+            visible << ["#{ActsAsTaggableOn::Tag.table_name}.name LIKE ?", "%#{name_like.downcase}%"]
+          end
+
+          options[:conditions] = visible.conditions
+          self.all_tag_counts(options)
         end
       end
 
