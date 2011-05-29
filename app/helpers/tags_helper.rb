98a11c30ec52f44abc058de14716dc9df581be05
Aleksey V Zapparov <ixti@member.fsf.org>
2011-05-29 23:22:38 +0200

    Resolves #36

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -19,6 +19,50 @@
 module TagsHelper
   include ActsAsTaggableOn::TagsHelper
 
+  # returns link to the page with issues filtered by specified filters
+  # === parameters
+  # * <i>title</i> = link title text
+  # * <i>filters</i> = filters to be applied (see <tt>link_to_filter_options</tt> for details)
+  # * <i>options</i> = (optional) base options of the link
+  # === example
+  # link_to_filter 'foobar', [[ :tags, '~', 'foobar' ]]
+  # link_to_filter 'foobar', [[ :tags, '~', 'foobar' ]], :project_id => project
+  def link_to_filter(title, filters, options = {})
+    options.merge! link_to_filter_options(filters)
+    link_to title, options
+  end
+
+
+  # returns hash suitable for passing it to the <tt>to_link</tt>
+  # === parameters
+  # * <i>filters</i> = array of arrays. each child array is an array of strings:
+  #                    name, operator and value
+  # === example
+  # link_to 'foobar', link_to_filter_options [[ :tags, '~', 'foobar' ]]
+  #
+  # filters = [[ :tags, '~', 'bazbaz' ], [:status_id, 'o']]
+  # link_to 'bazbaz', link_to_filter_options filters
+  def link_to_filter_options(filters)
+    options = {
+      :controller => 'issues',
+      :action => 'index',
+      :set_filter => 1,
+      :fields => [],
+      :values => {},
+      :operators => {}
+    }
+
+    filters.each do |f|
+      name, operator, value = f
+      options[:fields].push(name)
+      options[:operators][name] = operator
+      options[:values][name] = [value]
+    end
+
+    options
+  end
+
+
   # Returns tag link
   # === Parameters
   # * <i>tag</i> = Instance of Tag
