f6a68975e63ad5921f55ad00ff18e136882a6843
Ben Asher <basher@mobiata.com>
2012-02-24 15:54:02 -0500

    added hooks to support bulk edits

--- lib/redmine_tags/hooks/model_issue_hook.rb
+++ lib/redmine_tags/hooks/model_issue_hook.rb
@@ -24,6 +24,10 @@ module RedmineTags
         save_tags_to_issue(context, true)
       end
 
+      def controller_issues_bulk_edit_before_save(context={})
+        save_tags_to_issue(context, true)
+      end
+
       # Issue has an after_save method that calls reload (update_nested_set_attributes)
       # This makes it impossible for a new record to get a tag_list, it's
       # cleared on reload. So instead, hook in after the Issue#save to update
