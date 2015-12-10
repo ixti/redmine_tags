67aef1183c3f04f1e5e60ce243ddcb1b46357cab
Marius Balteanu <marius.balteanu@zitec.com>
2015-12-10 12:41:00 +0200

    Bump up version

--- init.rb
+++ init.rb
@@ -23,7 +23,7 @@ Redmine::Plugin.register :redmine_tags do
   name        'Redmine Tags'
   author      'Aleksey V Zapparov AKA "ixti"'
   description 'Redmine tagging support'
-  version     '3.0.0'
+  version     '3.1.0'
   url         'https://github.com/ixti/redmine_tags/'
   author_url  'http://www.ixti.net/'
 
