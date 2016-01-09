988f84cd15cdd07fc29d4c330040482a92548f0e
Codruț Constantin Gușoi <codrut.gusoi@gmail.com>
2016-01-09 15:29:07 +0200

    Rewrote unit tests in rspec

--- lib/redmine_tags/patches/wiki_page_patch.rb
+++ lib/redmine_tags/patches/wiki_page_patch.rb
@@ -16,8 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with redmine_tags.  If not, see <http://www.gnu.org/licenses/>.
 
-require_dependency 'wiki_page'
-
 module RedmineTags
   module Patches
     module WikiPagePatch
--- spec/models/issue_patch_spec.rb
+++ spec/models/issue_patch_spec.rb
@@ -5,4 +5,73 @@ describe Issue, type: :model do
     patch = RedmineTags::Patches::IssuePatch
     expect(Issue.included_modules).to include(patch)
   end
+
+  def create_issue(project, tags, author, tracker, status)
+    issue = build(
+      :issue,
+      project: project,
+      author:  author,
+      tracker: tracker,
+      status:  status
+    )
+    issue.tag_list = tags
+    issue.save
+    issue
+  end
+
+  context 'with default settings' do
+    let(:author)        { create :user }
+    let(:priority)      { create :issue_priority }
+    let(:status_open)   { create :issue_status }
+    let(:status_closed) { create :issue_status, is_closed: true }
+    let(:tracker)       { create :tracker, default_status_id: status_open.id }
+    let(:project_1) do
+      project = create :project
+      project.trackers = [tracker]
+      project.save
+      project
+    end
+    let(:project_2) do
+      project = create :project
+      project.trackers = [tracker]
+      project.save
+      project
+    end
+
+    before :example do
+      allow(User).to receive(:current).and_return(author)
+      create_issue(project_1, %w{a1 a2}, author, tracker, status_open)
+      create_issue(project_1, %w{a2 a3}, author, tracker, status_open)
+      create_issue(project_1, %w{a4 a5}, author, tracker, status_closed)
+      create_issue(project_2, %w{b6 b7}, author, tracker, status_closed)
+      create_issue(project_2, %w{b8 b9}, author, tracker, status_open)
+    end
+
+    it 'returns a list of distinct tags' do
+      expect(Issue.available_tags.size).to eq(9)
+    end
+
+    it 'allows listing tags of open issues only' do
+      expect(Issue.available_tags(open_only: true).size).to eq(5)
+    end
+
+    it 'allows listing tags of specific project only' do
+      expect(Issue.available_tags(project: project_1).size).to eq(5)
+      expect(Issue.available_tags(project: project_2).size).to eq(4)
+      expect(Issue.available_tags(open_only: true, project: project_1).size).to eq(3)
+      expect(Issue.available_tags(open_only: true, project: project_2).size).to eq(2)
+    end
+
+    it 'allows listing tags found by name' do
+      expect(Issue.available_tags(name_like: 'a').size).to eq(5)
+      expect(Issue.available_tags(name_like: 'b').size).to eq(4)
+      expect(Issue.available_tags(name_like: 'a1').size).to eq(1)
+      expect(Issue.available_tags(name_like: 'a2').size).to eq(1)
+
+      expect(Issue.available_tags(name_like: 'a', project: project_1).size).to eq(5)
+      expect(Issue.available_tags(name_like: 'b', project: project_1).size).to eq(0)
+      expect(Issue.available_tags(name_like: 'a', open_only: true, project: project_1).size).to eq(3)
+      expect(Issue.available_tags(name_like: 'b', open_only: true, project: project_1).size).to eq(0)
+    end
+  end
 end
