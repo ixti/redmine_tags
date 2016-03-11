8db2e3276da1eaeda44a78f32ef446bb939a3d00
Codruț Constantin Gușoi <codrut.gusoi@zitec.com>
2016-03-11 11:33:35 +0200

    Checking out faliures as well

--- spec/javascripts/simple_spec.js
+++ spec/javascripts/simple_spec.js
@@ -2,4 +2,8 @@ describe('the truth', function () {
   it('is true', function () {
     expect(truth()).to.equal(true);
   });
+
+  it('fails', function () {
+    expect(truth()).to.equal(false);
+  });
 });
