34572851abbd8c8fad23095d69d183c97f9b5885
Ben <uaqben@opmbx.org>
2016-06-07 15:17:15 +0200

    Removed line jump

--- lib/redmine_tags/hooks/views_issues_hook.rb
+++ lib/redmine_tags/hooks/views_issues_hook.rb
@@ -3,8 +3,7 @@ module RedmineTags
     class ViewsIssuesHook < Redmine::Hook::ViewListener
       render_on :view_issues_show_details_bottom, partial: 'issues/tags'
       render_on :view_issues_form_details_bottom, partial: 'issues/tags_form'
-      render_on :view_issues_sidebar_planning_bottom,
-        partial: 'issues/tags_sidebar'
+      render_on :view_issues_sidebar_planning_bottom, partial: 'issues/tags_sidebar'
     end
   end
 end
