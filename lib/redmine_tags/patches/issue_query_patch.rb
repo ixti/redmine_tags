87ee803ecf0138360005ded20e22d473ce52904a
Marius BALTEANU <marius.balteanu@zitec.com>
2017-04-16 02:56:20 +0000

    better fix for 3.4.0 version

--- lib/redmine_tags/patches/issue_query_patch.rb
+++ lib/redmine_tags/patches/issue_query_patch.rb
@@ -4,25 +4,16 @@ module RedmineTags
       def self.included(base)
         base.send :include, InstanceMethods
         base.class_eval do
-          alias_method :statement_original, :statement
-          alias_method :statement, :statement_extended
-          alias_method :available_filters_original, :available_filters
-          alias_method :available_filters, :available_filters_extended
+          unloadable
 
-          base.add_available_column QueryColumn.new(:tags)
+          alias_method_chain :available_filters, :tags
+          alias_method_chain :available_columns, :tags
         end
       end
 
       module InstanceMethods
-        def statement_extended
-          if filters
-            filter  = filters.delete 'tags'
-          end
-          clauses = statement_original || ''
-          if filter
-            filters.merge! 'tags' => filter
-            op = operator_for 'tags'
-            case op
+        def sql_for_tags_field(field, operator, value)
+          case operator
             when '=', '!'
               issues = Issue.tagged_with(values_for('tags'), any: true)
             when '!*'
@@ -30,24 +21,31 @@ module RedmineTags
             else
               issues = Issue.tagged_with ActsAsTaggableOn::Tag.all.map(&:to_s), any: true
             end
-            compare = op.eql?('!') ? 'NOT IN' : 'IN'
+            compare = operator.eql?('!') ? 'NOT IN' : 'IN'
             ids_list = issues.collect {|issue| issue.id }.push(0).join(',')
-            clauses << " AND " unless clauses.empty?
-            clauses << "( #{ Issue.table_name }.id #{ compare } (#{ ids_list }) ) "
-          end
-          clauses
+
+            "( #{ Issue.table_name }.id #{ compare } (#{ ids_list }) )"
         end
 
-        def available_filters_extended
-          unless @available_filters
-            filter = QueryFilter.new('tags',
-              type: :list_optional,
-              values: Issue.available_tags(project: project).collect {|t| [t.name, t.name]}
-            )
-
-            available_filters_original.merge!('tags' => filter)
+        def available_filters_with_tags
+          if @available_filters.blank?
+            add_available_filter('tags', :type => :list_optional, :name => l(:field_tags),
+              :values => Issue.available_tags(project: project).collect {|t| [t.name, t.name]}
+            ) if !available_filters_without_tags.key?('tags')
+          else
+            available_filters_without_tags
           end
           @available_filters
+        end
+
+        def available_columns_with_tags
+          if @available_columns.blank?
+            @available_columns = available_columns_without_tags
+            @available_columns << QueryColumn.new(:tags)
+          else
+            @available_columns_without_tags
+          end
+          @available_columns
         end
       end
     end
